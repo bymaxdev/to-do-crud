3b2615d485d70866d305b75c9f01c9b770036234
Max Reis <maxreisdossantos@hotmail.com>
2024-10-15 21:13:34 -0300

    Crud with DOM

--- js/script.js
+++ js/script.js
@@ -1,12 +1,55 @@
 const addListBtn = document.getElementById('addTaskBtn');
+const todoInput = document.getElementById("newTaskInput");
+const todoList = document.getElementById("taskList");
+
+
+function addTask() {
+    const task = todoInput.value.trim();
+
+    const addNewTask = document.createElement('li');
+    const checkBox = document.createElement('input');
+    const deleteBtn = document.createElement('a');
+    const taskText = document.createElement('span');
+
+    checkBox.style.marginRight = '10px';
+    checkBox.type = 'checkbox';
+
+    deleteBtn.textContent = "X";
+    deleteBtn.style.color = 'red';
+    deleteBtn.style.cursor = 'pointer';
+    deleteBtn.style.marginLeft = '10px';
+
+    if (task === '') {
+        alert('Please enter a task');
+        return;
+    };
+
+    taskText.textContent = task;
+
+    addNewTask.appendChild(checkBox);
+    addNewTask.appendChild(taskText);
+    addNewTask.appendChild(deleteBtn);
+
+    todoList.appendChild(addNewTask);
+
+    todoInput.value = '';
+
+    checkBox.addEventListener('click', () => {
+        if (checkBox.checked) {
+            taskText.style.textDecoration = 'line-through';
+        } else {
+            taskText.style.textDecoration = 'none';
+        }
+    });
+
+    deleteBtn.addEventListener('click', () => {
+        addNewTask.remove();
+    });
+}
 
 addListBtn.addEventListener('click', () => {
-    const task = document.getElementById('newTaskInput').value;
-    const taskList = document.getElementById('taskList');
-    const newTask = document.createElement('li');
-    newTask.textContent = task;
-    taskList.appendChild(newTask);
-    document.getElementById('newTaskInput').value = '';
+    addTask();
+    todoInput.value = '';
 });
 
 
